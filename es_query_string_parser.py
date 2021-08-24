78bbf2fc7060a4dc3a52753315df3e5f6ecd8ab8
ryook <ryo.kato23@gmail.com>
2021-08-24 22:33:17 +0900

    init

---
+++ es_query_string_parser.py
@@ -0,0 +1,15 @@
+from exceptions import ValidationError
+from parser import parser
+
+
+class EsQueryStringParser:
+
+    def __init__(self) -> None:
+        pass
+
+    def validate(string: str) -> bool:
+        try:
+            parser(string)
+        except Exception as e:
+            raise ValidationError(e)
+        return True
