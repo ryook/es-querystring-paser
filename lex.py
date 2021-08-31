c2b07ee3c265ecac01396dcffe1f9e74c10f16f3
ryook <ryo.kato23@gmail.com>
2021-08-31 18:43:23 +0900

    set token

--- lex.py
+++ lex.py
@@ -1,31 +1,46 @@
 import ply.lex as lex
 
 tokens = (
-    'NUMBER',
     'PLUS',
     'MINUS',
-    'TIMES',
-    'DIVIDE',
     'LPAREN',
     'RPAREN',
     'AND',
     'OR',
     'NOT',
+    'FIELD',
+    'WILDCARDS',
+    'REGEXP',
+    'FUZZINESS',
+    'LRANGE',
+    'RRANGE',
+    'NUMBER',
+    'ALPHABET'
 )
 
 t_PLUS = r'\+'
 t_MINUS = r'-'
-t_TIMES = r'\*'
-t_DIVIDE = r'/'
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
 t_AND = r'AND'
 t_OR = r'OR'
 t_NOT = r'NOT'
+t_FIELD = r':'
+t_WILDCARDS = r'[?|*]'
+t_REGEXP = r'/'
+t_FUZZINESS = r'~'
+t_LRANGE = r'[\[|{]'
+t_RRANGE = r'[\]|}]'
+
 
 def t_NUMBER(t):
     r'\d+'
     t.value = int(t.value)
+    return t
+
+def t_ALPHABET(t):
+    r'[a-zA-Z]+'
+    t.value = str(t.value)
     return t
 
 def t_newline(t):
@@ -40,19 +55,18 @@ def t_error(t):
 
 lexer = lex.lex()
 
-data = '''
-A OR B AND (C AND D)
-'''
+if __name__ == "__main__":
+    data = 'A OR B AND (C AND DD*)'
 
-lexer.input(data)
+    lexer.input(data)
 
-while True:
-    tok = lexer.token()
-    if not tok:
-        break
-    print(tok)
-    print(tok.type)
-    print(tok.value)
-    print(tok.lineno)
-    print(tok.lexpos)
+    while True:
+        tok = lexer.token()
+        if not tok:
+            break
+        print(tok)
+        print(tok.type)
+        print(tok.value)
+        print(tok.lineno)
+        print(tok.lexpos)
 
